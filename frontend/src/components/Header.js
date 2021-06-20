b9f251e5a5bf9ad70df19c9396e2394f6d5a544f
C Hoekstra <c_hoekstra@comcast.net>
2021-06-20 11:57:13 -0500

    controlled search commp

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Navbar, Container } from 'react-bootstrap';
+
+
+const navbarStyle = {
+  backgroundColor: 'lightblue'
+};
+
+const Header = ({title}) => {
+//const title = props.title;
+return (
+    <Navbar style={ navbarStyle }  variant="light">
+   <Container>
+    <Navbar.Brand href="/">{title}</Navbar.Brand>
+</Container>
+  </Navbar>
+)
+
+};
+
+export default Header;
