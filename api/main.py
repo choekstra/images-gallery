651ccf15b1e8e4247b3e836f27e4888ea052020f
C Hoekstra <c_hoekstra@comcast.net>
2021-06-24 17:46:00 -0500

    sample flask app

--- api/main.py
+++ api/main.py
@@ -2,5 +2,22 @@
 # create virtual env: 
 # flask dependency
 
+# save this as app.py
+from flask import Flask
+#from other_module import fn_from_other_module
 
+#print(__name__)
+#fn_from_other_module()
+def hello():
+    return "Hello, World!"
 
+app = Flask(__name__)
+
+#app.route("/")(hello)
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+if __name__ == "__main__": 
+    app.run(host="0.0.0.0", port=5050)
---
+++ api/other_module.py
@@ -0,0 +1,5 @@
+def fn_from_other_module():
+    print("from other")
+    print(__name__)
+
+fn_from_other_module()
